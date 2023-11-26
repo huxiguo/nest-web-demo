2a48f0cd6131e42b4ee1624c700936ff81b38425
huxiguo <hu@seekhoo.cn>
2023-11-26 10:05:59 +0800

    session-l

--- src/views/NestSessionView.vue
+++ src/views/NestSessionView.vue
@@ -5,12 +5,26 @@ const labelPosition = ref<FormProps['labelPosition']>('right')
 
 const formLabelAlign = reactive({
   name: '',
-  region: '',
-  type: ''
+  password: '',
+  code: ''
 })
 
+const codeUrl = ref<string>('/api/validate-code/code')
 const onSubmit = () => {
-  console.log(123)
+  fetch('/api/validate-code/create', {
+    method: 'post',
+    body: JSON.stringify(formLabelAlign),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+}
+
+const resetCode = () => {
+  const time = new Date().getTime()
+  codeUrl.value = codeUrl.value + `?${time}`
 }
 </script>
 <template>
@@ -20,16 +34,19 @@ const onSubmit = () => {
       label-width="100px"
       :model="formLabelAlign"
       style="max-width: 460px"
-      size="large s"
+      size="large"
     >
       <el-form-item label="账号">
         <el-input v-model="formLabelAlign.name" />
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formLabelAlign.region" />
+        <el-input v-model="formLabelAlign.password" type="password" />
       </el-form-item>
       <el-form-item label="验证码">
-        <el-input v-model="formLabelAlign.type" />
+        <div style="display: flex">
+          <el-input v-model="formLabelAlign.code" />
+          <img :src="codeUrl" alt="验证码" @click="resetCode" />
+        </div>
       </el-form-item>
       <el-form-item>
         <el-button type="default" @click="onSubmit">登录</el-button>
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,16 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   plugins: [
     vue(),
     AutoImport({
